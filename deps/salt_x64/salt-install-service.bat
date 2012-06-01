697a5c6460b7b40fc5c78736cbcd867acb7a2197
Baiju Muthukadan <baiju.m.mail@gmail.com>
2012-06-01 10:36:58 +0000

    Batch files for install & update

---
+++ deps/salt_x64/salt-install-service.bat
@@ -0,0 +1,4 @@
+C:
+cd \salt\python\App
+
+"C:\\salt\\python\\App\\python" "C:\\salt\\python\\App\\Scripts\\salt-minion" -c "c:\\salt\\etc\\minion" -d --pid-file "c:\\salt\\var\\run\\salt-minion.pid"
